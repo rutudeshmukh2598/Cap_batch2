8da033be296e666c30de11d12b3ac7fa77a97976
rutuja <rutudeshmukh2598@gmail.com>
2022-03-15 12:38:36 -0400

    day3 folder added

---
+++ day3/c5_p1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+#define exp1 num%10
+#define exp2 num=num/10
+
+int main()
+{
+   int num=123,sum=0;
+   
+   int r=0;
+   
+   sum=sum +exp1;
+   printf("\n sum at 1st step=%d\n",sum);
+   exp2;
+   sum=sum+exp1;
+   printf("\n sum aat 2nd step=%d\n",sum);
+   exp2;
+   sum=sum+ exp1;
+   printf("\n sum at 3 step=%d num=%d\n",sum);
+   exp2;
+   sum = sum +exp1;
+   printf("\n sum at 4 step=%d num=%d\n",sum);
+   
+   printf("\n sum=%d \n ",sum);
+   return 0;
+   }
+   
