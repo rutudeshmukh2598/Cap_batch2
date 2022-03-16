cb0471ba9d581958d4cdf0e31f9748f54766d810
rutuja <rutudeshmukh2598@gmail.com>
2022-03-16 02:52:10 -0400

    primeNo.c added in day4

---
+++ day4/primeNo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int no=7;
+  int temp=0;
+  for(int i=2;i<=no-1;i++)
+  {
+     if(no%2==0)
+     {
+        temp=temp + 1;
+        }
+    }
+    if(temp==0)
+    {
+       printf("NO is prime\n");
+     }
+     else
+     {
+       printf("No is not prime\n");
+     }
+     return 0;
+  }           
