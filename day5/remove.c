a102001bc3108b0505b464eea58881c7fc5851df
rutuja <rutudeshmukh2598@gmail.com>
2022-03-17 01:47:05 -0400

    remove.c file added in day5 folder

---
+++ day5/remove.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+   int  arr[7]={1,2,3,4,2,5,1};
+   int i,j,k,temp,n=7;
+   
+   for(i=0;i<n;i++)
+   {
+      for(j=i+1;j<n;j++)
+      {
+        if(arr[i]==arr[j])
+        {
+           for(k=j;k<n;k++)
+           {
+              arr[k]=arr[k+1];
+              }
+              n--;
+              j--;
+              }
+              }
+           }
+           for(i=0;i<n;i++)
+           printf("%d",arr[i]);
+   }        
