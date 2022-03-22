30f6ecaf50fd296a20fd33cdaf809585263c6a37
rutuja <rutudeshmukh2598@gmail.com>
2022-03-22 13:21:29 -0400

    p4.c added in brance_Rutuja

---
+++ cprogramming/day8/p4.c
@@ -0,0 +1,17 @@
+/*
+
+  register storage class
+  */
+  
+  #include<stdio.h>
+  int main()
+  {
+  int i;
+  int a=10;
+  for(i=0;i<10;i++)
+   printf(" %d",i);
+   
+   printf("\n%d",a);
+   printf("\n\n");
+   return 0;
+   }
