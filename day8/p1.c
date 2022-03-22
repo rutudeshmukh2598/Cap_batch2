f90169055df0331352244804b2cdc3f468620e93
rutuja <rutudeshmukh2598@gmail.com>
2022-03-22 01:12:13 -0400

    p1.c added in day8 folder

---
+++ day8/p1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void swapNos(int *,int *);
+
+int main()
+{
+   int a=10,b=20;
+  printf("\nPass by value call\n");
+   swapNos(25,30);
+  
+   printf("\na=%d\nb=%d\n",a,b);
+   
+   printf("\nPass by address call\n");
+  swapNos(&a,&b);
+  
+   printf("\na=%d\nb=%d\n",a,b);
+   return 0;
+   }
+ void swapNos(int *ptr1, int *ptr2)
+   {
+       int temp;
+       temp=*ptr1;
+       *ptr1=*ptr2;
+       *ptr2=temp;
+       
+       
+       
+       }
+       
