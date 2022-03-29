92857f1c7073c2c7784cd30f65e2ebf8bc32388a
rutuja <rutudeshmukh2598@gmail.com>
2022-03-29 01:41:29 -0400

    Updated

---
+++ cprogramming/day13/l1.c
@@ -0,0 +1,48 @@
+/*
+
+demo linked list
+*/
+#include<stdio.h>
+ 
+ struct list
+ {
+   int val;
+   struct list *ptr;
+   };
+   
+   typedef struct list LIST;
+   
+   int main()
+   {
+     LIST n1,n2,n3;
+     LIST *pn;
+     
+     
+     
+     n1.val=10;
+     n1.ptr=NULL;   //ground
+     
+     n2.val=20;
+     n2.ptr=NULL;
+     
+     n3.val=30;
+     n3.ptr=NULL;
+     
+     n1.ptr=&n2;
+     n2.ptr=&n3;
+     
+     pn=&n1;
+     
+     printf("\n %d",pn->val);
+     
+     pn=&n2;
+     
+     printf("\n %d",pn->val);
+     
+     pn=&n3;
+     printf("\n %d",pn->val);
+     
+     
+     printf("\nNULL\n");
+     
+     }
