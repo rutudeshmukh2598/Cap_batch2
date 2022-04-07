5bdb1d951f347f5b62110c99ab4784e0614e78d4
rutuja <rutudeshmukh2598@gmail.com>
2022-04-07 06:29:33 -0400

    day1 added in c++programming

--- c++programming/day1/p4.cpp
+++ c++programming/day1/p4.cpp
@@ -1,1 +1,18 @@
-//program to 
+//program to  demo srand and rand function
+
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+int main()
+{
+   //for(int i=0;i<5;i++)
+   // cout<<rand()<<endl;
+    
+    srand(time(0));
+    
+    for(int i=0;i<5;i++)
+    cout<<rand()<<endl;
+    return 0;
+    }
