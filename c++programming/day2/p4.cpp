c7168c54ff411459b18f9468eb3b379edd750f64
rutuja <rutudeshmukh2598@gmail.com>
2022-04-08 02:02:21 -0400

    Updated

---
+++ c++programming/day2/p4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+char str[]="Unable to allocate memory";
+cerr<<"Error Message : "<<str<<endl;
+clog<<"Logged with error :"<<str<<endl;
+}
---
+++ c++programming/day2/p5.cpp
@@ -0,0 +1,28 @@
+//demo on class and objects
+
+#include<iostream>
+
+using namespace std;
+
+
+  
+  class emp_details
+  {
+  public:
+      int empid;
+      char empname[20];
+  
+  };
+  
+  int main()
+  {
+      emp_details e1,e2;
+      e1.empid=1001;
+      
+    cout<<e1.empid;
+     cout<<e2.empid;
+      
+     
+     
+     return 0;
+     }
